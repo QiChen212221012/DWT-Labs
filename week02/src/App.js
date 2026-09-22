9715fea9efaba5b8a247e695b35317962efacb36
QiChen212221012 <164645315+QiChen212221012@users.noreply.github.com>
2026-09-22 01:28:56 +0100

    Add files via upload

---
+++ week02/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import GradeToolbar from "./GradeToolBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>DWT: Week 02 - Challenge 04</h1>
+      <GradeToolbar />
+    </div>
+  );
+}
---
+++ week02/src/GradeToolBar.js
@@ -0,0 +1,77 @@
+function Button({ onClick, children, style, onMouseOver, onMouseOut }) {
+  return (
+    <button
+      onClick={onClick}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+      style={style}
+    >
+      {children}
+    </button>
+  );
+}
+
+function PassedButton() {
+  function handleMouseOver(event) {
+    event.target.style.backgroundColor = "lightgreen";
+    event.target.style.color = "darkgreen";
+  }
+
+  function handleMouseOut(event) {
+    event.target.style.backgroundColor = "white";
+    event.target.style.color = "green";
+  }
+
+  return (
+    <Button
+      onClick={() => alert("You have passed the module!")}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      style={{
+        color: "green",
+        padding: "10px",
+        border: "1px solid green",
+        borderRadius: "5px",
+      }}
+    >
+      Passed
+    </Button>
+  );
+}
+
+function FailedButton() {
+  function handleMouseOver(event) {
+    event.target.style.backgroundColor = "lightcoral";
+    event.target.style.color = "darkred";
+  }
+
+  function handleMouseOut(event) {
+    event.target.style.backgroundColor = "white";
+    event.target.style.color = "red";
+  }
+
+  return (
+    <Button
+      onClick={() => alert("Student failed the module!")}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+      style={{
+        color: "red",
+        padding: "10px",
+        border: "1px solid red",
+        borderRadius: "5px",
+      }}
+    >
+      Failed
+    </Button>
+  );
+}
+
+export default function GradeToolbar() {
+  return (
+    <div>
+      <PassedButton />
+      <FailedButton />
+    </div>
+  );
+}
